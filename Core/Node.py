d33ab907c8935399e6ee8ec6d8052b2a17f8333f
Freddeb <f.bredmar@gmail.com>
2015-09-06 15:28:52 +0200

    Removed neighbours variable from the Node

--- Core/Node.py
+++ Core/Node.py
@@ -5,3 +5,8 @@
 # * Current block
 # * Neighbours
 # * Shrunken world state
+class Node:
+    world = [[0 for x in range(20)] for x in range(10)]
+    block = None
+#    neighbours = None
+    smallworld = None # Might be better to have a function for this
